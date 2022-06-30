c37727e0a6f47604f94d25fe9a33aa31285dbd07
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-06-30 00:23:10 -0300

    Add initial Spring Boot server files
    Spring Boot will be used as a REST API for the app.

---
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/CompetitiveProgrammingAppApplication.kt
@@ -0,0 +1,11 @@
+package com.gustavo.competitiveprogrammingapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompetitiveProgrammingAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<CompetitiveProgrammingAppApplication>(*args)
+}
