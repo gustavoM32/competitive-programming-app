8e420298963c5a146b854112a8202e00332c9b6c
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-07-28 07:06:20 -0300

    Expose resources ids in the REST api
    https://github.com/spring-projects/spring-data-rest/issues/547#issuecomment-752906632

---
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/config/RestConfiguration.kt
@@ -0,0 +1,30 @@
+package com.gustavo.competitiveprogrammingapp.config
+
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.type.filter.RegexPatternTypeFilter
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import java.util.regex.Pattern
+
+@Configuration
+class RestConfiguration : RepositoryRestConfigurer {
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration, cors: CorsRegistry) {
+        val provider = ClassPathScanningCandidateComponentProvider(false)
+        provider.addIncludeFilter(RegexPatternTypeFilter(Pattern.compile(".*")))
+
+        val beans = provider.findCandidateComponents("com.gustavo.competitiveprogrammingapp.domain")
+
+        for (bean in beans) {
+            var idExposedClasses: Class<*>?
+            try {
+                idExposedClasses = Class.forName(bean.beanClassName)
+                config.exposeIdsFor(Class.forName(idExposedClasses.name))
+            } catch (e: ClassNotFoundException) {
+                // Can't throw ClassNotFoundException due to the method signature. Need to cast it
+                throw RuntimeException("Failed to expose `id` field due to", e)
+            }
+        }
+    }
+}
