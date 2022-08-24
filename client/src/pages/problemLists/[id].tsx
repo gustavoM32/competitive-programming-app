83ec51d75713e46f41452f2416e3f44ae288abab
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-08-24 17:48:14 -0300

    Add button to update data in problem list page

--- client/src/pages/problemLists/[id].tsx
+++ client/src/pages/problemLists/[id].tsx
@@ -1,12 +1,15 @@
-import { Link } from '@mui/material'
+import { Button, Link } from '@mui/material'
 import { UpdateProblemListDialog, DeleteProblemListButtonOne } from 'components/problemListCRUD'
 import { API_URL } from 'constants/constants'
 import { useRead } from 'hooks/crudHooks'
 import { useRouter } from 'next/router'
+import { useQueryClient } from "@tanstack/react-query"
 
 export default function ProblemList() {
+  const queryClient = useQueryClient()
   const { query } = useRouter()
-  const problemListData = useRead(query?.id != undefined ? `${API_URL}/problemLists/${query.id}` : "")
+  const resourceURI = query?.id != undefined ? `${API_URL}/problemLists/${query.id}` : ""
+  const problemListData = useRead(resourceURI)
 
   if (problemListData.isLoading) return <p>Loading...</p>
   if (problemListData.error || !problemListData.data) {
@@ -20,6 +23,7 @@ export default function ProblemList() {
     <>
       <h1>Problem list</h1>
       <h2>{problemList.name}</h2>
+      <Button onClick={() => queryClient.invalidateQueries([resourceURI]) }>Update data</Button>
       <Link href={problemList.link} target="_blank" rel="noopener">Link</Link>
       <p>{problemList.description}</p>
       <p>{problemList.notes}</p>
