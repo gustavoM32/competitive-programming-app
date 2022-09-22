179e1c556c0a42eef5d025545491cb0ea0a396d0
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-09-21 22:34:21 -0300

    Add status map to problems page

--- client/src/pages/Problems.tsx
+++ client/src/pages/Problems.tsx
@@ -3,14 +3,44 @@ import { DataGrid } from "@mui/x-data-grid";
 import { CreateProblemDialog, UpdateProblemDialog, DeleteProblemButton } from "components/problemCRUD";
 import { useReadList } from "hooks/crudHooks";
 import { UpdateDataButton } from "components/general";
+import { Fragment } from "react";
 
 type RowParams = {
   id: any,
   row: any
 }
 
+const problemStatusMap : { [key: string]: string } = {
+  "NOTHING": "Not read",
+  "READ": "Read before",
+  "WA": "WA",
+  "AC": "Accepted"
+};
+
+const editorialStatusMap : { [key: string]: string } = {
+  "NOTHING": "Not read",
+  "READ_BEFORE_AC": "Read before",
+  "READ_AFTER_AC": "Read after",
+};
+
 export default function Problems() {
   const problems = useReadList(["problems"]);
+
+  const convertStatus = (field: string, map: { [key: string]: string }) => {
+    return (params: RowParams) => {
+      return map[params.row[field]];
+    }
+  }
+
+  const convertLinesToBr = (field: string) => {
+    return (params: RowParams) => {
+      const text = params.row[field];
+      const result = text.split('\n').map((item: string, key: string) => {
+        return (<Fragment key={key}>{item}<br/></Fragment>)
+      });
+      return result;
+    }
+  }
 
   const columns = [
     { field: 'dateAdded', headerName: 'Date added', type: 'dateTime', width: 250},
@@ -20,11 +50,11 @@ export default function Problems() {
     { field: 'name', headerName: 'Name', width: 200},
     // { field: 'rating', headerName: 'Rating'},
     // { field: 'topics', headerName: 'Topics'},
-    { field: 'problemStatus', headerName: 'Status', width: 150},
-    { field: 'editorialStatus', headerName: 'Editorial', width: 200},
-    { field: 'comments', headerName: 'Comments', width: 400},
+    { field: 'problemStatus', headerName: 'Status', width: 150, renderCell: convertStatus('problemStatus', problemStatusMap) },
+    { field: 'editorialStatus', headerName: 'Editorial', width: 200, renderCell: convertStatus('editorialStatus', editorialStatusMap)},
+    { field: 'comments', headerName: 'Comments', width: 400, renderCell: convertLinesToBr('comments')},
     // { field: 'studies', headerName: 'Studies'},
-    { field: 'action', headerName: 'Action', width: 200, renderCell: (params: RowParams) => (
+    { field: 'action', headerName: 'Action', width: 300, renderCell: (params: RowParams) => (
       <>
         <UpdateProblemDialog problem={params.row}/>{' '}
         <DeleteProblemButton id={params.id}/>
