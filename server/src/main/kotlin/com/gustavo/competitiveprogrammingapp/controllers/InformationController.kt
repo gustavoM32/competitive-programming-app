ec44f9796d65a48bde689a24a0de785570943ea6
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-09-30 00:53:03 -0300

    Add problemsetProblems to InformationController
    The GET calls to server /info path will trigger a call to CF api to update server data.

---
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/controllers/InformationController.kt
@@ -0,0 +1,20 @@
+package com.gustavo.competitiveprogrammingapp.controllers
+
+import com.gustavo.competitiveprogrammingapp.cfApi.processors.ProblemsetProblemsProcessor
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("info")
+class InformationController(val problemsetProblemsProcessor: ProblemsetProblemsProcessor) {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("problemsetProblems")
+    fun getProblemsetProblems(): String {
+        problemsetProblemsProcessor.update()
+        return "OK"
+    }
+}
