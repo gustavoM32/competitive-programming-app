4010a2480013f25105a5976edbce0bb6107e20cd
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-09-30 00:09:43 -0300

    Add ProblemsetProblems processor

---
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/cfApi/processors/ProblemsetProblemsProcessor.kt
@@ -0,0 +1,29 @@
+package com.gustavo.competitiveprogrammingapp.cfApi.processors
+
+import com.gustavo.competitiveprogrammingapp.cfApi.resources.ResourceFetcher
+import com.gustavo.competitiveprogrammingapp.domain.cfProblem.CfProblem
+import com.gustavo.competitiveprogrammingapp.domain.cfProblem.CfProblemRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class ProblemsetProblemsProcessor(val resourceFetcher: ResourceFetcher, val repository: CfProblemRepository):
+    Processor {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    override fun update() {
+        val problemsetProblems = resourceFetcher.getProblemSetProblems()
+        val problems = problemsetProblems.problems
+
+        repository.deleteAll() // FIXME: deleting to avoid multiple entries, find a way to not need that
+        logger.info("Drop database")
+        problems?.let { ps ->
+            val cfp = ps.map { p ->
+                CfProblem(null, p.contestId.toString() + p.index, p.name, 0)
+            }
+            repository.saveAll(cfp)
+        }
+        logger.info("Updated database")
+    }
+}
---
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/cfApi/processors/Processor.kt
@@ -0,0 +1,6 @@
+package com.gustavo.competitiveprogrammingapp.cfApi.processors
+
+/* Processors update the server database given API structured data */
+interface Processor {
+    fun update()
+}
