6e624317a5e1eb8c0c176cb344860d3a8e89e5b1
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-09-30 00:54:43 -0300

    Add button component to call updateInfo

--- client/src/components/general.tsx
+++ client/src/components/general.tsx
@@ -1,10 +1,24 @@
 import { Button } from "@mui/material";
 import { useQueryClient } from "@tanstack/react-query";
+import { updateInfo } from "api/updateInfo";
 
 export function UpdateDataButton() {
   const queryClient = useQueryClient()
   
   return (
     <Button variant="outlined" onClick={() => queryClient.invalidateQueries() }>Update data</Button>
+  )
+}
+
+export function UpdateCfDataButton(props: { infoPath: string }) {
+  const queryClient = useQueryClient()
+
+  const updateCfData = () => {
+    updateInfo(props.infoPath)
+      .then(() => queryClient.invalidateQueries())
+  }
+  
+  return (
+    <Button variant="outlined" onClick={updateCfData}>Update CF data</Button>
   )
 }
