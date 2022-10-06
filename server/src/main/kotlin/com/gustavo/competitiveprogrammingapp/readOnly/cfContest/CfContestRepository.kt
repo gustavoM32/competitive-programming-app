2a42e989e1b3a4f05acd36a5c8daed03c39bcdb3
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-10-06 18:33:01 -0300

    Change CfContestRepository key type to Int

--- server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/readOnly/cfContest/CfContestRepository.kt
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/readOnly/cfContest/CfContestRepository.kt
@@ -1,7 +1,7 @@
 package com.gustavo.competitiveprogrammingapp.readOnly.cfContest
 
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@Repository
-interface CfContestRepository : MongoRepository<CfContest, String>
+@RepositoryRestResource
+interface CfContestRepository : MongoRepository<CfContest, Int>
