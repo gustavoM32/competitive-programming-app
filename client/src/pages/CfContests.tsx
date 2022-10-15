40c4af3b7a97bf1ca99fa1e4b40428386c8377c6
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-10-15 13:28:21 -0300

    Add filters draft to cf contests page

--- client/src/pages/CfContests.tsx
+++ client/src/pages/CfContests.tsx
@@ -3,6 +3,26 @@ import { Grid, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import DataGrid from "components/DataGrid";
 import { useReadList } from "hooks/crudHooks";
 
+// const Inputs = () => {
+//   return (
+//     <Grid>
+//     <TextField label="Name">Name</TextField>
+//     <InputLabel id="demo-simple-select-label">Type</InputLabel>
+//     <Select>
+//       <MenuItem value={10}>Codeforces Round</MenuItem>
+//       <MenuItem value={20}>Educational Round</MenuItem>
+//       <MenuItem value={30}>Global Round</MenuItem>
+//     </Select>
+//     <InputLabel id="demo-simple-select-label">Div</InputLabel>
+//     <Select>
+//       <MenuItem value={1}>1</MenuItem>
+//       <MenuItem value={2}>2</MenuItem>
+//       <MenuItem value={3}>3</MenuItem>
+//       <MenuItem value={4}>4</MenuItem>
+//     </Select>
+//   </Grid>
+//   )
+// }
 
 export default function CfContests() {
   const cfContests = useReadList(["cfContests"]);
