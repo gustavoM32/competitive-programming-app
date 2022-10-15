f6e9bcc46167cd313ab713a6c6c79faa0c1af24d
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-10-15 13:27:48 -0300

    Add name and status filter to cf problems page
    Result are currently limited to 200 to make development faster.

--- client/src/pages/CfProblems.tsx
+++ client/src/pages/CfProblems.tsx
@@ -2,9 +2,17 @@ import { UpdateDataButton, UpdateCfDataButton } from "components/general";
 import DataGrid from "components/DataGrid";
 import { useReadList, useReadPage } from "hooks/crudHooks";
 import 'styles/globals.css'
+import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from "@mui/material";
+import { useState } from "react";
 
 export default function CfProblems() {
-  const cfProblems = useReadPage(["cfProblemWithUserStatuses"], 0, 200);
+  const cfProblems = useReadPage(["cfProblemWithUserStatuses"], 0, 200); // FIXME: Fetch all
+  const [queryName, setQueryName] = useState("");
+  const [statusFilter, setStatusFilter]: any = useState({
+    nothing: true,
+    wa: true,
+    ac: true
+  });
   
   const columns = [
     {
@@ -20,17 +28,71 @@ export default function CfProblems() {
   ];
 
   const getRowClass = (row: any) => {
-    console.log(row);
     const problem = row.data
     const acs = problem.submissions.filter((p: any) => p.verdict === "OK");
     if (acs.length > 0) return "ac-color";
     if (problem.submissions.length > 0) return "wa-color";
+    return "";
   }
+
+  const handleChange = (e: any) => {
+    setQueryName(e.target.value);
+  }
+
+  const handleStatusChange = (e: any) => {
+    const statusName = e.target.name;
+    setStatusFilter({
+      ...statusFilter,
+     [statusName]: e.target.checked
+    });
+  }
+
+  const filteredCfProblems = cfProblems.resources.filter((p: any) => {
+    const lowercasedName = p.name.toLowerCase();
+    if (!lowercasedName.includes(queryName)) return false;
+    
+    let lowercasedStatus: string = getRowClass({ data: p })
+    if (lowercasedStatus === "ac-color") lowercasedStatus = "ac";
+    else if (lowercasedStatus === "wa-color") lowercasedStatus = "wa";
+    else lowercasedStatus = "nothing";
+    if (!statusFilter[lowercasedStatus]) return false;
+
+    return true
+  })
 
   return (
     <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        >
+        <Grid item>
+          <TextField
+            label="Nome"
+            name="query"    
+            value={queryName}
+            onChange={handleChange}/>
+        </Grid>
+        
+        <Grid item>
+          <FormControl
+            component="fieldset"
+            sx={{ m: 3 }}
+            variant="standard"
+          >
+            <FormLabel>Status</FormLabel>
+            <FormGroup>
+              <FormControlLabel control={<Checkbox checked={statusFilter.nothing} onChange={handleStatusChange} name="nothing" />} label="Nothing" />
+              <FormControlLabel control={<Checkbox checked={statusFilter.wa} onChange={handleStatusChange} name="wa" />} label="WA" />
+              <FormControlLabel control={<Checkbox checked={statusFilter.ac} onChange={handleStatusChange} name="ac" />} label="AC" />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+      </Grid>
       <DataGrid
-        rowData={cfProblems.resources}
+        rowData={filteredCfProblems}
         columnDefs={columns}
         getRowClass={getRowClass}
       />
