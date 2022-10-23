c7b41fffca36466362e3797e9cea444029252533
Gustavo Carlos <gustavo.dmcarlos@gmail.com>
2022-10-22 21:33:54 -0300

    Add Timer check

--- server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/util/Timer.kt
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/util/Timer.kt
@@ -6,18 +6,32 @@ import java.time.Duration
 import java.time.LocalDateTime
 
 class Timer(private var msg: String = "timer") {
+    private var checkNum = 1;
+
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
-    var startTime: LocalDateTime? = null
+    private var startTime: LocalDateTime? = null
+    private var checkTime: LocalDateTime? = null
 
-    fun start() {
+    private fun start() {
         startTime = LocalDateTime.now()
+        checkTime = startTime
+    }
+
+    fun check(name: String?) {
+        val now = LocalDateTime.now()
+        val checkDur = Duration.between(checkTime, now).toString()
+        val totalDur = Duration.between(startTime, now).toString()
+        logger.info("$msg - check $checkNum ($name) $checkDur     Total: $totalDur")
+
+        checkTime = now
+        checkNum += 1
     }
 
     fun stop() {
         val endTime = LocalDateTime.now()
         val dur = Duration.between(startTime, endTime).toString()
-        logger.info("$msg: $dur")
+        logger.info("$msg $dur")
     }
 
     init {
