81acba01ce4a2d833bfad9278b8f207a3586fadb
Gustavo Carlos <gustavo.dmcarlos@gmail.com>
2022-12-09 10:05:46 -0300

    Allow using different server port and domain

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,3 +1,10 @@
-export const SERVER_URL = "http://localhost:8080";
+const DEV_MODE =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+// Change domain and port for production
+export const SERVER_DOMAIN = DEV_MODE ? "localhost" : "localhost";
+export const SERVER_PORT = DEV_MODE ? "8080" : "8080";
+
+export const SERVER_URL = `http://${SERVER_DOMAIN}:${SERVER_PORT}`;
 export const API_URL = `${SERVER_URL}/api`;
 export const INFO_URL = `${SERVER_URL}/info`;
