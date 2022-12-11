59c910aa511070d192c9c5c5a60325902841c1d1
Gustavo Carlos <gustavo.dmcarlos@gmail.com>
2022-12-11 09:48:47 -0300

    Rename UpdateController to InformationController

--- server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/information/InformationController.kt
+++ server/src/main/kotlin/com/gustavo/competitiveprogrammingapp/information/InformationController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("info")
-class UpdateController(
+class InformationController(
     val informationService: InformationService,
     val cfContestProcessor: CfContestProcessor,
     val cfGymContestProcessor: CfGymContestProcessor,
